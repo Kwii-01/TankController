ab811a5bc2ad981c76d50225a03237808c32ecd3
Kwii <alexandrecaila26@gmail.com>
2024-01-11 22:22:33 +0100

    WIP - turret

--- Assets/Src/Scripts/Vehicles/Tank/Turret.cs
+++ Assets/Src/Scripts/Vehicles/Tank/Turret.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
-public class Turret : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
+namespace Vehicles {
+    public class Turret : MonoBehaviour {
+        [SerializeField] private Transform _weaponTransform;
+        [SerializeField] private float _rotationSpeed;
+        [SerializeField] private Vector2 _clampX;
+        private Quaternion _targetRotation;
+        private Quaternion _targetWeaponRotation;
 
-    }
+        private void Start() {
+            this._targetRotation = this.transform.rotation;
+        }
 
-    // Update is called once per frame
-    void Update() {
+        private void Update() {
+            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, this._targetRotation, this._rotationSpeed * Time.deltaTime);
+        }
 
+        public void Rotate(Vector3 direction) {
+
+            float angle = Quaternion.Angle(Quaternion.Euler(0f, direction.y, 0f), this.transform.localRotation);
+            if (angle > this._clampX.x && angle < this._clampX.y) {
+                _targetWeaponRotation = Quaternion.Euler(0f, direction.y, 0f);
+            }
+            // ONLY Y ROTATION
+            direction.y = 0f;
+            this._targetRotation = Quaternion.LookRotation(direction.normalized);
+        }
+
+        public void BeginShooting() {
+
+        }
+
+        public void StopShooting() {
+
+        }
     }
 }
