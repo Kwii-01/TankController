0309d7efacdfa9ba8a25af733722110e69a09ae7
Kwii <alexandrecaila26@gmail.com>
2024-01-16 12:23:58 +0100

    FIX - acceleration

--- Assets/Src/Scripts/Vehicles/Tank/Tank.cs
+++ Assets/Src/Scripts/Vehicles/Tank/Tank.cs
@@ -4,8 +4,10 @@ using System.Linq;
 
 using UnityEngine;
 
-namespace Vehicles {
-    public class Tank : Vehicle {
+namespace Vehicles
+{
+    public class Tank : Vehicle
+    {
         private static readonly float DEFAULT_SPEED_MULTIPLIER = 1f;
         [Header("Tank")]
         [SerializeField] private float _accelerationTime;
@@ -28,40 +30,50 @@ namespace Vehicles {
         private float _currentSpeed;
         private float _normalizedSpeed;
 
-        private void Start() {
+        private void Start()
+        {
             this._turret.Setup(this);
             this._leftSpeedMultiplier = DEFAULT_SPEED_MULTIPLIER;
             this._rightSpeedMultiplier = DEFAULT_SPEED_MULTIPLIER;
             this._accelerationTimer = this._accelerationTime;
         }
 
-        private void Update() {
-            if (this.Speeding) {
-                if (this._accelerationTimer < this._accelerationTime) {
+        private void Update()
+        {
+            if (this.Speeding)
+            {
+                if (this._accelerationTimer < this._accelerationTime)
+                {
                     this._accelerationTimer += Time.deltaTime;
                 }
             }
         }
 
-        protected void FixedUpdate() {
+        protected void FixedUpdate()
+        {
             this.ApplyForcesOnWheels();
             this.ClampSpeed();
         }
 
-        public override void Move(float direction) {
-            if (this.isBlackout == false) {
+        public override void Move(float direction)
+        {
+            if (this.isBlackout == false)
+            {
                 this._power = this.statBehaviour.Get(StatSystem.StatType.Acceleration) * direction;
-                if (this.Speeding == false && direction != 0) {// JUST BEGAN TO SPEED
+                if (this.Speeding == false && direction != 0)
+                {// JUST BEGAN TO SPEED
                     this._accelerationTimer = 0f;
                 }
                 this.Speeding = direction != 0f;
-                if (this.Speeding == false) {
+                if (this.Speeding == false)
+                {
                     this.Stop();
                 }
             }
         }
 
-        public override void Stop() {
+        public override void Stop()
+        {
             this._power = 0f;
             this.Speeding = false;
             this._accelerationTimer = this._accelerationTime;
@@ -69,59 +81,76 @@ namespace Vehicles {
             this._rightSpeedMultiplier = DEFAULT_SPEED_MULTIPLIER;
         }
 
-        public override void Steer(float direction) {
-            if (this._power == 0f) {
+        public override void Steer(float direction)
+        {
+            if (this._power == 0f)
+            {
                 this.SetSteer(DEFAULT_SPEED_MULTIPLIER, -DEFAULT_SPEED_MULTIPLIER, direction);
-            } else {
+            }
+            else
+            {
                 this.SetSteer(2f, DEFAULT_SPEED_MULTIPLIER, direction);
             }
         }
 
-        private void SetSteer(float steerSuperior, float steerInferior, float direction) {
-            if (direction > 0) {
+        private void SetSteer(float steerSuperior, float steerInferior, float direction)
+        {
+            if (direction > 0)
+            {
                 this.Steering = true;
                 this._leftSpeedMultiplier = steerSuperior;
                 this._rightSpeedMultiplier = steerInferior;
-            } else if (direction < 0) {
+            }
+            else if (direction < 0)
+            {
                 this.Steering = true;
                 this._leftSpeedMultiplier = steerInferior;
                 this._rightSpeedMultiplier = steerSuperior;
-            } else {
+            }
+            else
+            {
                 this.Steering = false;
                 this._leftSpeedMultiplier = DEFAULT_SPEED_MULTIPLIER;
                 this._rightSpeedMultiplier = DEFAULT_SPEED_MULTIPLIER;
             }
         }
 
-        public bool IsGrounded() {
+        public bool IsGrounded()
+        {
             return this._leftChain.IsGrounded() && this._rightChain.IsGrounded();
         }
 
-        public override float GetNormalizedSpeed() {
+        public override float GetNormalizedSpeed()
+        {
             return this._normalizedSpeed;
         }
 
-        private void ApplyForcesOnWheels() {
+        private void ApplyForcesOnWheels()
+        {
             float speed = this.CalculateSpeed();
-            if (this.Steering && this.Speeding == false) {
+            if (this.Steering && this.Speeding == false)
+            {
                 speed = this.statBehaviour.Get(StatSystem.StatType.Acceleration) / 1.5f;
             }
             this._leftChain.Apply(speed * this._leftSpeedMultiplier);
             this._rightChain.Apply(speed * this._rightSpeedMultiplier);
         }
 
-        private void ClampSpeed() {
+        private void ClampSpeed()
+        {
             Vector3 velocity = this.transform.InverseTransformDirection(this.rb.velocity);
             float maxSpeed = this.statBehaviour.Get(StatSystem.StatType.MoveSpeed);
             velocity.z = Mathf.Clamp(velocity.z, -maxSpeed, maxSpeed);
             this.rb.velocity = this.transform.TransformDirection(velocity);
         }
 
-        private float CalculateSpeed() {
+        private float CalculateSpeed()
+        {
             this._currentSpeed = Vector3.Dot(this.transform.forward, this.rb.velocity);
             this._normalizedSpeed = Mathf.Clamp01(Mathf.Abs(this._currentSpeed) / this.statBehaviour.Get(StatSystem.StatType.MoveSpeed));
-            if (this.Speeding) {
-                return this._accelerationCurve.Evaluate(this._accelerationTime / this._accelerationTimer) * this._power;
+            if (this.Speeding)
+            {
+                return this._accelerationCurve.Evaluate(this._accelerationTimer / this._accelerationTime) * this._power;
             }
             return -this._currentSpeed * this.rb.mass * 2f;
         }
